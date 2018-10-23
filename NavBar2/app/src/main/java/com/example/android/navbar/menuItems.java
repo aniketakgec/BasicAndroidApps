34ddf47abaf5342619da9e3c6d7fe37cd475a2b6
aniketakgec <aniketpandey353@gmail.com>
2018-10-24 02:20:11 +0530

    simple navigation drawer

---
+++ NavBar2/app/src/main/java/com/example/android/navbar/menuItems.java
@@ -0,0 +1,13 @@
+package com.example.android.navbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menuItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_items);
+    }
+}
